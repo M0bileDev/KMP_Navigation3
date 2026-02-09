4f8e6c48b624f8c6fe02dc096fcb8876498688ee
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-10 00:23:27 +0100

    feature(screens): provide ListScreen screen
    Created simple screen with list

---
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/screens/ListScreen.kt
@@ -0,0 +1,42 @@
+package com.example.kmp_navigation3.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kmp_navigation3.viewmodels.ListScreenViewModel
+
+@Composable
+fun TodoList(
+    viewModel: ListScreenViewModel = viewModel(),
+    onClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val todos by viewModel.todos.collectAsStateWithLifecycle()
+
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(todos) { todo ->
+            Text(
+                text = todo,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onClick(todo)
+                    }
+                    .padding(16.dp)
+            )
+        }
+    }
+}
