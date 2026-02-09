4a9df2df8e16e5b4d03f4a579d75bc117d2a7983
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-10 00:03:11 +0100

    feature(gradle): implement libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
@@ -30,7 +30,6 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
-            implementation(libs.koin.android)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -42,17 +41,10 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
 
-            implementation(libs.nav3.runtime)
-            implementation(libs.nav3.ui)
-            implementation(libs.androidx.lifecycle.viewmodel.nav3)
-            implementation(libs.kotlinx.serialization.core)
+            implementation(libs.jetbrains.lifecycle.viewmodel)
+            implementation(libs.jetbrains.lifecycle.viewmodel.nav3)
+            implementation(libs.jetbrains.navigation3.ui)
             implementation(libs.kotlinx.serialization.json)
-
-            implementation(libs.androidx.compose.material3.windowsizeclass)
-            implementation(libs.androidx.material3.adaptive)
-
-            implementation(libs.koin.core)
-            implementation(libs.koin.androidx.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
