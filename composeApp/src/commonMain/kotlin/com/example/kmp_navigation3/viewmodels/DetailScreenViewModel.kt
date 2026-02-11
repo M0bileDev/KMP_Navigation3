42f53d7f31bd5f2b5f8e4680f0d0d34aea1d745f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-11 01:32:39 +0100

    refactor(viewmodels): provide logs for debugging

--- composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/viewmodels/DetailScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/viewmodels/DetailScreenViewModel.kt
@@ -4,12 +4,24 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
+private const val TAG = "DetailScreenViewModel"
+
 class DetailScreenViewModel(
     todo: String
 ) : ViewModel() {
 
     private val _detailState = MutableStateFlow(DetailState(todo))
     val detailState get() = _detailState.asStateFlow()
+    
+    init {
+        println("$TAG Initialize view model: $this -> todo value: $todo")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        println("$TAG Clear view model -> todo value: $this")
+
+    }
 }
 
 data class DetailState(
