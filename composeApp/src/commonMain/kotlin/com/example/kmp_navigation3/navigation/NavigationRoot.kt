539ac2b5e31552a429b9e05aefcc294f5d433e97
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-11 01:07:15 +0100

    feature(navigation): provide initial setup of navigation

---
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationRoot.kt
@@ -0,0 +1,53 @@
+package com.example.kmp_navigation3.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import androidx.savedstate.serialization.SavedStateConfiguration
+import com.example.kmp_navigation3.screens.DetailScreen
+import com.example.kmp_navigation3.screens.ListScreen
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+@Composable
+fun NavigationRoot() {
+    val navBackStack = rememberNavBackStack(
+        configuration = SavedStateConfiguration {
+            //defines how routes should be serialized
+            serializersModule = SerializersModule {
+                //set up polymorphic serializer, to provide proper serialization mechanism
+                polymorphic(NavKey::class) {
+                    subclass(Route.ListScreen::class, Route.ListScreen.serializer())
+                    subclass(Route.DetailScreen::class, Route.DetailScreen.serializer())
+                }
+            }
+        }, Route.ListScreen
+    )
+
+    NavDisplay(
+        backStack = navBackStack,
+        entryProvider = { key ->
+            when (key) {
+                is Route.ListScreen -> {
+                    NavEntry(key) {
+                        ListScreen(
+                            onClick = {
+                                // TODO: navigate to detail screen
+                            }
+                        )
+                    }
+                }
+
+                is Route.DetailScreen -> {
+                    NavEntry(key) {
+                        DetailScreen(key.todo)
+                    }
+                }
+
+                else -> error("Unknown NavKey; $key")
+            }
+        }
+    )
+}
