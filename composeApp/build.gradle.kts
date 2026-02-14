096bfa683660badad3f8b7c5c47095d2e6f72337
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-14 01:34:52 +0100

    feature(gradle): implement material icons library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
 
+            implementation(libs.material.icons.extended)
+
             implementation(libs.jetbrains.lifecycle.viewmodel)
             implementation(libs.jetbrains.lifecycle.viewmodel.nav3)
             implementation(libs.jetbrains.navigation3.ui)
