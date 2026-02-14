56c2570da51f9ecd303268141adee1742eda8c67
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-14 01:37:13 +0100

    refactor(navigation): provide view updates
    Implemented scaffold with bottom bar, updated navigation

--- composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationRoot.kt
@@ -1,6 +1,12 @@
 package com.example.kmp_navigation3.navigation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavKey
@@ -29,26 +35,54 @@ fun NavigationRoot(modifier: Modifier = Modifier) {
         }, Route.ListScreen
     )
 
-    NavDisplay(
-        modifier = modifier,
-        backStack = navBackStack,
-        entryDecorators = listOf(
-            //
-            rememberSaveableStateHolderNavEntryDecorator(),
-            //viewmodels will be scoped properly
-            rememberViewModelStoreNavEntryDecorator()
-        ),
-        entryProvider = entryProvider {
-            entry<Route.ListScreen> {
-                ListScreen(
-                    onClick = { todo ->
-                        navBackStack.add(Route.DetailScreen(todo))
-                    }
-                )
-            }
-            entry<Route.DetailScreen> {
-                DetailScreen(it.todo)
-            }
+    Scaffold(
+        bottomBar = {
+            TodoNavigationBar(
+                selectKey = Route.ListScreen,
+                onKeyChange = {
+                    // TODO: provide implementation
+                }
+            )
         }
-    )
+    ) { innerPadding ->
+        NavDisplay(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            backStack = navBackStack,
+            entryDecorators = listOf(
+                //
+                rememberSaveableStateHolderNavEntryDecorator(),
+                //viewmodels will be scoped properly
+                rememberViewModelStoreNavEntryDecorator()
+            ),
+            entryProvider = entryProvider {
+                entry<Route.ListScreen> {
+                    ListScreen(
+                        onClick = { todo ->
+                            navBackStack.add(Route.DetailScreen(todo))
+                        }
+                    )
+                }
+                entry<Route.DetailScreen> {
+                    DetailScreen(it.todo)
+                }
+                entry<Route.TodoFavorites> {
+                    ListScreen(
+                        onClick = { todo ->
+                            navBackStack.add(Route.DetailScreen(todo))
+                        }
+                    )
+                }
+                entry<Route.Settings> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Settings")
+                    }
+                }
+            }
+        )
+    }
 }
