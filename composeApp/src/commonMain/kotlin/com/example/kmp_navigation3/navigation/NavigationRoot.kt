5803d5e9ff9b96260a7ade2fd9a508cd4b1e5eee
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-15 02:57:32 +0100

    feature(navigation): code restructure
    Implemented NavDisplay for entries constructor config, removed previous backstack config

--- composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationRoot.kt
@@ -8,31 +8,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
-import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
-import androidx.navigation3.runtime.rememberNavBackStack
-import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
-import androidx.savedstate.serialization.SavedStateConfiguration
+import com.example.kmp_navigation3.navigation.BottomNavItem.Companion.topLevelDestinations
 import com.example.kmp_navigation3.screens.DetailScreen
 import com.example.kmp_navigation3.screens.ListScreen
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.polymorphic
 
 @Composable
 fun NavigationRoot(modifier: Modifier = Modifier) {
-    val navBackStack = rememberNavBackStack(
-        configuration = SavedStateConfiguration {
-            //defines how routes should be serialized
-            serializersModule = SerializersModule {
-                //set up polymorphic serializer, to provide proper serialization mechanism
-                polymorphic(NavKey::class) {
-                    subclass(Route.ListScreen::class, Route.ListScreen.serializer())
-                    subclass(Route.DetailScreen::class, Route.DetailScreen.serializer())
-                }
-            }
-        }, Route.ListScreen
+    val navigationState = rememberNavigationState(
+        startRoute = Route.ListScreen,
+        topLevelRoutes = topLevelDestinations.keys
     )
 
     Scaffold(
@@ -45,44 +31,41 @@ fun NavigationRoot(modifier: Modifier = Modifier) {
             )
         }
     ) { innerPadding ->
+
         NavDisplay(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            backStack = navBackStack,
-            entryDecorators = listOf(
-                //
-                rememberSaveableStateHolderNavEntryDecorator(),
-                //viewmodels will be scoped properly
-                rememberViewModelStoreNavEntryDecorator()
-            ),
-            entryProvider = entryProvider {
-                entry<Route.ListScreen> {
-                    ListScreen(
-                        onClick = { todo ->
-                            navBackStack.add(Route.DetailScreen(todo))
+            onBack = {},
+            entries = navigationState.toEntries(
+                entryProvider = entryProvider {
+                    entry<Route.ListScreen> {
+                        ListScreen(
+                            onClick = { todo ->
+                                navBackStack.add(Route.DetailScreen(todo))
+                            }
+                        )
+                    }
+                    entry<Route.DetailScreen> {
+                        DetailScreen(it.todo)
+                    }
+                    entry<Route.TodoFavorites> {
+                        ListScreen(
+                            onClick = { todo ->
+                                navBackStack.add(Route.DetailScreen(todo))
+                            }
+                        )
+                    }
+                    entry<Route.Settings> {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Settings")
                         }
-                    )
-                }
-                entry<Route.DetailScreen> {
-                    DetailScreen(it.todo)
-                }
-                entry<Route.TodoFavorites> {
-                    ListScreen(
-                        onClick = { todo ->
-                            navBackStack.add(Route.DetailScreen(todo))
-                        }
-                    )
-                }
-                entry<Route.Settings> {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text("Settings")
                     }
                 }
-            }
+            )
         )
     }
 }
