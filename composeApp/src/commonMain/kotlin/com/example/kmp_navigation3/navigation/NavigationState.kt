1a38bd74dc2434ea84522a2a9070db298139baf7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-15 02:11:36 +0100

    feature(navigation): provide composable type of NavigationState.kt

--- composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationState.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/NavigationState.kt
@@ -1,10 +1,20 @@
 package com.example.kmp_navigation3.navigation
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSerializable
 import androidx.compose.runtime.setValue
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.savedstate.compose.serialization.serializers.MutableStateSerializer
+import androidx.savedstate.serialization.SavedStateConfiguration
+import kotlinx.serialization.PolymorphicSerializer
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
 
 class NavigationState(
     // first visible destination
@@ -40,4 +50,48 @@ class NavigationState(
                 topLevelRoute
             )
         }
+}
+
+@Composable
+fun rememberNavigationState(
+    startRoute: NavKey,
+    topLevelRoutes: Set<NavKey>
+): NavigationState {
+    val topLevelRoute = rememberSerializable(
+        startRoute,
+        topLevelRoutes,
+        configuration = configuration,
+        serializer = MutableStateSerializer(PolymorphicSerializer(NavKey::class))
+    ) {
+        mutableStateOf(startRoute)
+    }
+
+    val backStacks = topLevelRoutes.associateWith { key ->
+        rememberNavBackStack(
+            configuration = configuration,
+            //first destination per feature
+            key
+        )
+    }
+
+    return remember(startRoute, topLevelRoutes) {
+        NavigationState(
+            startRoute = startRoute,
+            topLevelRoute = topLevelRoute,
+            backStacks = backStacks
+        )
+    }
+}
+
+val configuration = SavedStateConfiguration {
+    //defines how routes should be serialized
+    serializersModule = SerializersModule {
+        //set up polymorphic serializer, to provide proper serialization mechanism
+        polymorphic(NavKey::class) {
+            subclass(Route.ListScreen::class, Route.ListScreen.serializer())
+            subclass(Route.DetailScreen::class, Route.DetailScreen.serializer())
+            subclass(Route.TodoFavorites::class, Route.TodoFavorites.serializer())
+            subclass(Route.Settings::class, Route.Settings.serializer())
+        }
+    }
 }
