baba2a203b7909934c7fd4cf07768f1759f3aad5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-17 23:22:10 +0100

    feature(scenes): provide implementation for strategy scope

--- composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/scenes/ListDetailSceneStrategy.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmp_navigation3/navigation/scenes/ListDetailSceneStrategy.kt
@@ -16,6 +16,27 @@ class ListDetailSceneStrategy<T : Any>(
         //must be at least medium size screen width
         if (!windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) return null
 
-        // TODO: provide impl
+        //need to know which one of the screens need to be positioned as list and detail screen
+        //detail (last one on the backstack) and list(element before last on the backstack)
+
+        val detailEntry = entries
+            .lastOrNull()
+            ?.takeIf {
+                //check if scene has a specific key (like detail)
+                it.metadata.containsKey(ListDetailScene.DETAIL_KEY)
+            } ?: return null
+
+        val listEntry = entries
+            .findLast {
+                it.metadata.containsKey(ListDetailScene.LIST_KEY)
+            } ?: return null
+
+        return ListDetailScene(
+            list = listEntry,
+            detail = detailEntry,
+            key = listEntry.contentKey,
+            // when navigate back, where should it end up
+            previousEntries = entries.dropLast(1)
+        )
     }
 }
